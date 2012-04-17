d64fb3a5708094a08f64e8fafd0db54a762219ee
Galina Troshchenko <galafit@gmail.com>
2012-04-17 16:31:57 +0200

    Getters and setters were added in Model

--- src/com/github/dreamrec/Model.java
+++ src/com/github/dreamrec/Model.java
@@ -6,10 +6,42 @@ import java.util.List;
  *
  */
 public class Model {
-    List<Integer> inData;
-    double frequency;
-    long startTime;
-    int viewIndex;
+    private ListView<Integer> eyeDataList;
+    private double frequency;
+    private long startTime;
+    private int viewIndex;
+
+    public List<Integer> getEyeDataList() {
+        return eyeDataList;
+    }
+
+    public void addEyeData(int data) {
+        eyeDataList.add(data);
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getViewIndex() {
+        return viewIndex;
+    }
+
+    public void setViewIndex(int viewIndex) {
+        this.viewIndex = viewIndex;
+    }
 }
 
 
