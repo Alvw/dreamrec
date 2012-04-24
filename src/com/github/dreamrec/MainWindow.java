891675951276c882fb2e91aae6e7e232f76364b5
Alvw <flashvw@gmail.com>
2012-04-24 18:41:22 +0400

    MainWindow in progress

--- src/com/github/dreamrec/MainWindow.java
+++ src/com/github/dreamrec/MainWindow.java
@@ -1,9 +1,13 @@
 package com.github.dreamrec;
 
+import javax.swing.*;
+
 /**
  *
  */
-public class MainWindow {
+public class MainWindow extends JFrame{
+
+
 
     public void repaint(){
         throw new UnsupportedOperationException("todo");
--- src/com/github/dreamrec/gcomponent/GComponentView.java
+++ src/com/github/dreamrec/gcomponent/GComponentView.java
@@ -2,22 +2,24 @@ package com.github.dreamrec.gcomponent;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
 
 /**
  *
  */
-public class GComponentView {
+public class GComponentView extends JPanel {
 
-    JPanel panel;
-    List<IPainter> painters;
+    private GComponentPainter componentPainter = new GComponentPainter();
+    private GComponentModel gModel;
 
-    private class GPanel extends JPanel{
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            // ������������� �� ��������� � �������� ����� paint(Graphics g);
-        }
+    public GComponentView(GComponentModel gModel) {
+        this.gModel = gModel;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D)g;
+        componentPainter.paint(g2d, gModel);
     }
 
 }
